844547f563ce58d7618e4d1a033d618d258ad6f6
Wiebe Kaai <wiebekaai@gmail.com>
2021-12-14 15:40:31 +0100

    ShouldHideHeader logic

---
+++ hooks/useDocumentScrollThrottled.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { throttle } from "underscore";
+
+const THROTTLE_WAIT = 500;
+
+const useDocumentScrollThrottled = (callback) => {
+  const [, setScrollPosition] = useState(0);
+  let previousScrollTop = 0;
+
+  const onScroll = () => {
+    const { scrollTop: currentScrollTop } =
+      document.documentElement || document.body;
+
+    setScrollPosition((previous) => {
+      previousScrollTop = previous;
+      return currentScrollTop;
+    });
+
+    callback({
+      previousScrollTop,
+      currentScrollTop,
+    });
+  };
+
+  const onScrollThrothled = throttle(onScroll, THROTTLE_WAIT);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScrollThrothled);
+    return () => window.removeEventListener("scroll", onScrollThrothled);
+  }, []);
+};
+
+export default useDocumentScrollThrottled;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import styled from "styled-components";
+import useDocumentScrollThrottled from "../hooks/useDocumentScrollThrottled";
+
+const BigContent = styled.main`
+  height: 3000px;
+`;
+
 export default function Home() {
-  return <>Hello world</>;
+  const [shouldHideHeader, setShouldHideHeader] = useState(false);
+
+  useDocumentScrollThrottled(({ previousScrollTop, currentScrollTop }) => {
+    const isScrolledDown = currentScrollTop > previousScrollTop;
+    const isMinimumScrolled = currentScrollTop > 3;
+    setShouldHideHeader(isScrolledDown && isMinimumScrolled);
+  });
+
+  console.log({ shouldHideHeader });
+
+  return (
+    <>
+      <BigContent />
+    </>
+  );
 }
