4d8e832204b1093aa85946281cf51156443d60af
Wiebe Kaai <wiebekaai@gmail.com>
2021-12-14 13:37:21 +0100

    ESLint + Prettier

---
+++ pages/_app.jsx
@@ -0,0 +1,6 @@
+import "../styles/globals.css";
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default MyApp;
