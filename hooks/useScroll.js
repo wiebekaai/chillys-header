2fa2a903bd1c7dc7e654ba081dfb4240b13c7731
Wiebe Kaai <wiebekaai@gmail.com>
2022-06-01 12:32:07 +0200

    Fix react import

--- hooks/useScroll.js
+++ hooks/useScroll.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import useDocumentScrollThrottled from "./useDocumentScrollThrottled";
 import useScrollLock from "./useScrollLock";
 
